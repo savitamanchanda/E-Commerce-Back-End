a8004eb5a224704b709503ff49a949eef7ac2845
Savita Rankavat <savita@Savitas-MacBook-Pro.local>
2023-02-14 13:03:22 -0600

    defined columns in models

--- models/index.js
+++ models/index.js
@@ -5,7 +5,9 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  
+})
 // Categories have many Products
 
 // Products belongToMany Tags (through ProductTag)
